03869013a8f1ffc63c7d7700e801fa8631429d6d
hoorak17 <hoorak17@naver.com>
2026-01-12 21:06:17 +0900

    feat: app navigation skeleton

--- android/app/src/main/java/com/example/plannermvp/MainActivity.kt
+++ android/app/src/main/java/com/example/plannermvp/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.plannermvp.ui.theme.PlannerMVPTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +25,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             PlannerMVPTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNav(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +33,73 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun AppNav(modifier: Modifier = Modifier) {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "home",
         modifier = modifier
-    )
+    ) {
+        composable("home") {
+            HomeScreen(onGoToday = { navController.navigate("today") })
+        }
+        composable("today") {
+            TodayScreen(onGoSummary = { navController.navigate("summary") })
+        }
+        composable("summary") {
+            SummaryScreen(onGoPlan = { navController.navigate("plan") })
+        }
+        composable("plan") {
+            PlanNextDayScreen(onGoHome = { navController.navigate("home") })
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(onGoToday: () -> Unit) {
+    Column {
+        Text("Home")
+        Button(onClick = onGoToday) {
+            Text("Go Today")
+        }
+    }
+}
+
+@Composable
+fun TodayScreen(onGoSummary: () -> Unit) {
+    Column {
+        Text("Today")
+        Button(onClick = onGoSummary) {
+            Text("Go Summary")
+        }
+    }
+}
+
+@Composable
+fun SummaryScreen(onGoPlan: () -> Unit) {
+    Column {
+        Text("Summary")
+        Button(onClick = onGoPlan) {
+            Text("Go Plan Next Day")
+        }
+    }
+}
+
+@Composable
+fun PlanNextDayScreen(onGoHome: () -> Unit) {
+    Column {
+        Text("Plan Next Day")
+        Button(onClick = onGoHome) {
+            Text("Back to Home")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun PreviewHome() {
     PlannerMVPTheme {
-        Greeting("Android")
+        HomeScreen {}
     }
 }
